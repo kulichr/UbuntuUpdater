c31ddeabfc722061cdc63723a2612c2f6e1fe023
Roman Kulich <rpkulich@gmail.com>
2020-11-30 21:42:52 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Roman Kulich @ 2020
+# Version: v0.0.1
+
+sudo apt update && apt list --upgradable
+echo " "
+echo -e "\e[92mDo you want to upgrade these apps?\e[39m"
+read upgrade
+if [[ $upgrade =~ "y" ]]; then
+   sudo apt upgrade
+else echo -e "\e[92mDone\e[39m"
+fi

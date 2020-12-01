7f1fca28d82759821bba5289ecabc308e0ab9b03
Roman Kulich <rpkulich@gmail.com>
2020-12-01 09:36:16 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,11 +2,15 @@
 # Author: Roman Kulich @ 2020
 # Version: v0.0.1
 
-sudo apt update && apt list --upgradable
-echo " "
+sudo apt update
+echo ""
+echo -e "\e[92mThese packages are upgradable:\e[39m"
+apt list --upgradable
+echo ""
 echo -e "\e[92mDo you want to upgrade these packages?\e[39m"
 read upgrade
 if [[ $upgrade =~ "y" ]]; then
    sudo apt upgrade
+   echo -e "\e[92mAll packages upgraded!\e[39m"
 else echo -e "\e[92mDone\e[39m"
 fi

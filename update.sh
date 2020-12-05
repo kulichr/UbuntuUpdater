26d723859165255166422db2bf8b567fa4662594
Roman <rpkulich@gmail.com>
2020-12-05 19:23:31 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ echo ""
 echo -e "\e[92mDo you want to upgrade these packages? (y/n)\e[39m"
 read upgrade
 if [[ $upgrade =~ "y" ]]; then
-   sudo apt upgrade
+   sudo apt upgrade -y
    echo -e "\e[92mAll packages upgraded!\e[39m"
 else echo -e "\e[92mDone\e[39m"
 fi
